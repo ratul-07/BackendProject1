00a78c77a96f28f7b4dcb95363ef53d453ea1cf2
RATUL SUR <sur.ratul.712@gmail.com>
2024-01-31 19:06:34 +0530

    app imported in index.js from app.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import express from "express"
+import { app } from "./app.js";
 
-const app = express()
+// const app = express()
+
 dotenv.config({
     path: './env'
 })
