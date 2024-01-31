ad31fcf844f2abacf37e63eccd5d53107378ced0
RATUL SUR <sur.ratul.712@gmail.com>
2024-01-31 18:16:03 +0530

    src/index.js app defined

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import express from "express"
 
+const app = express()
 dotenv.config({
     path: './env'
 })
+
+
 
 connectDB()
 .then(() => {
